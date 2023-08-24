901045cb8c577f0f73563aa5707e7486278fe526
Gloria Dal Santo <gloria.dalsanto@outlook.it>
2023-08-24 12:55:34 +0300

    Absorption coeff from material specs

--- ism_dataset.py
+++ ism_dataset.py
@@ -12,6 +12,72 @@ import pyroomacoustics as pra
 
 np.random.seed(0)
 
+# meatrial array 
+# walls 
+wall_mat = {
+"plasterboard" : {
+    "description": "2 * 13 mm plasterboard on steel frame, 50 mm mineral wool in cavity, surface painted",
+    "coeffs": [0.15, 0.10, 0.06, 0.04, 0.04, 0.05, 0.05], 
+    "center_freqs": [125, 250, 500, 1000, 2000, 4000, 8000],
+},
+"wooden_lining" : {
+    "description": "Wooden lining, 12 mm fixed on frame",
+    "coeffs": [0.27, 0.23, 0.22, 0.15, 0.10, 0.07, 0.06],
+    "center_freqs": [125, 250, 500, 1000, 2000, 4000, 8000],
+},
+"limestone_wall" : {
+    "description": "Limestone walls",
+    "coeffs": [0.02, 0.02, 0.03, 0.04, 0.05, 0.05, 0.05],
+    "center_freqs": [125, 250, 500, 1000, 2000, 4000, 8000],
+},
+"smooth_brickwork_10mm_pointing": {
+    "description": "Smooth brickwork, 10 mm deep pointing, pit sand mortar",
+    "coeffs": [0.08, 0.09, 0.12, 0.16, 0.22, 0.24, 0.24],
+    "center_freqs": [125, 250, 500, 1000, 2000, 4000, 8000],
+}
+}
+# floor
+floor_mat = {
+"audience_floor" : {
+    "description": "Audience floor, 2 layers, 33 mm on sleepers over concrete",
+    "coeffs": [0.09, 0.06, 0.05, 0.05, 0.05, 0.04],
+    "center_freqs": [125, 250, 500, 1000, 2000, 4000],
+},
+"carpet_cotton" : {
+        "description": "Cotton carpet",
+        "coeffs": [0.07, 0.31, 0.49, 0.81, 0.66, 0.54, 0.48],
+        "center_freqs": [125, 250, 500, 1000, 2000, 4000, 8000],
+},
+"carpet_hairy": {
+    "description": "Hairy carpet on 3 mm felt",
+    "coeffs": [0.11, 0.14, 0.37, 0.43, 0.27, 0.25, 0.25],
+    "center_freqs": [125, 250, 500, 1000, 2000, 4000, 8000],
+},
+"carpet_tufted_9m" : {
+    "description": "9 mm tufted pile carpet on felt underlay",
+    "coeffs": [0.08, 0.08, 0.30, 0.60, 0.75, 0.80, 0.80],
+    "center_freqs": [125, 250, 500, 1000, 2000, 4000, 8000],
+}
+}
+# ceiling
+ceiling_mat = {
+"ceiling_plasterboard" : {
+    "description": "Plasterboard ceiling on battens with large air-space above",
+    "coeffs": [0.20, 0.15, 0.10, 0.08, 0.04, 0.02],
+    "center_freqs": [125, 250, 500, 1000, 2000, 4000],
+},
+"ceiling_melamine_foam" : {
+    "description": "Wedge-shaped, melamine foam, ceiling tile",
+    "coeffs": [0.12, 0.33, 0.83, 0.97, 0.98, 0.95],
+    "center_freqs": [125, 250, 500, 1000, 2000, 4000],
+},
+"ceiling_metal_panel": {
+    "description": "Metal panel ceiling, backed by 20 mm Sillan acoustic tiles, panel width 85 mm, panel spacing 15 mm, cavity 35 cm",
+    "coeffs": [0.59, 0.80, 0.82, 0.65, 0.27, 0.23],
+    "center_freqs": [125, 250, 500, 1000, 2000, 4000],
+}
+}
+
 # room dimension
 room_dim_opt = {
     "small": {"length": [2, 7],"width": [1.5, 5], "height": [2.4, 3]},
@@ -38,7 +104,7 @@ def sample_room_dim(dim_limits):
     return np.round(np.multiply(room_dim,1000))/1000
 
 def sample_room_materials():
-    s = np.random.uniform(0.0, 0.5)
+    s = np.random.uniform(0.0, 0.2)
     a1 = np.random.uniform(0.0, 1)
     a2 = np.random.uniform(0.0, 1)
     a3 = np.random.uniform(0.0, 1)
@@ -53,6 +119,24 @@ def sample_room_materials():
     )  
     return materials, [s, a1, a2, a3]
 
+def sample_materials_from_dict():
+    wall_n = np.random.randint(0, len(wall_mat.keys()))
+    floor_n = np.random.randint(0, len(floor_mat.keys()))
+    ceiling_n = np.random.randint(0, len(ceiling_mat.keys()))
+    # get materials 
+    cm = list(ceiling_mat.keys())[ceiling_n]
+    fm = list(floor_mat.keys())[floor_n]
+    wm = list(wall_mat.keys())[wall_n]
+    materials = pra.make_materials(
+        ceiling=ceiling_mat[cm],
+        floor=floor_mat[fm],
+        east=wall_mat[wm],
+        west=wall_mat[wm],
+        north=wall_mat[wm],
+        south=wall_mat[wm],
+    )    
+    return materials, [cm, fm, wm]
+    
 def sample_room_interior(room_dim):
     x = np.random.uniform(0.01, room_dim[0]-0.01)
     y = np.random.uniform(0.01, room_dim[1]-0.01)
@@ -106,7 +190,7 @@ def main(args):
 
     # open metadata file 
     filepath = os.path.join(args.dir_path, 'metadata.csv')
-    fieldnames = ['filename', 'size', 'room_dim', 'materials', 'source', 'mic', 'rt60', 'max_order', 'ray_tracing', 'sr']
+    fieldnames = ['filename', 'size', 'room_dim', 'material_ceil', 'material_floor', 'material_wall', 'source', 'mic', 'rt60', 'max_order', 'ray_tracing', 'sr']
     with open(filepath, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()        
@@ -122,7 +206,7 @@ def main(args):
             # sample the shoebox room configuration parameters 
             # dimension
             config['room_dim'] = sample_room_dim(room_dim_opt[room_type])
-            config['materials'], materials = sample_room_materials()
+            config['materials'], materials = sample_materials_from_dict()
             config['source'] = sample_room_interior(config['room_dim'])
             config['mic'] = sample_room_interior(config['room_dim'])
             
@@ -136,8 +220,11 @@ def main(args):
             shoebox.compute_rir()
             rir = shoebox.rir[0][0].copy()
             config['rt60'] = shoebox.measure_rt60(decay_db=60)
-            config['materials'] = np.round(np.multiply(materials,1000))/1000 # overwrite for the csv file
+            config['material_ceil'] = materials[0] 
+            config['material_floor'] = materials[1]
+            config['material_wall'] = materials[2]
             config['size'] = room_type
+            del config['materials']
 
             # save file 
             config['filename'] = os.path.join(args.dir_path, str(uuid.uuid4()) + '.wav')
@@ -150,7 +237,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--n_rir', default = 200000, type=int,
         help = 'Number of RIRs to be simulated')
-    parser.add_argument('--sr', default = 44100, type=int,
+    parser.add_argument('--sr', default = 48000, type=int,
         help='Sampling rate')
     parser.add_argument('--split',  nargs='+', type=float,
         help='room size contribution in parts - samll, medium, large')
